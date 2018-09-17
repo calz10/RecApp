8b6e3b84eed69db67977349696d88e5c7f51a88a
Israel Calderon <caldz@Israels-MacBook-Air.local>
2018-09-18 00:18:04 +0800

    Initial Commit

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const AppContainer = () => {
+  return (
+    <App/>
+  )
+}
+
+ReactDOM.render(<AppContainer />, document.getElementById('root'))
